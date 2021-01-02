e5b478186da47293dfb2dc277ab638277d3e5728
codejaeger <mandaldebabrata123@gmail.com>
2021-01-02 20:58:03 +0530

    Update server code

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 var express = require("express");
+const request = require('request');
 
 var app = express();
 
-app.get("/", (req, res, next) => {
+const options = {  
+    url: 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=25bfd122210c448c912246ee9c0a27f3',
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+    }
+};
 
-    fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=25bfd122210c448c912246ee9c0a27f3')
-    .then(response => response.json())
-    .then(data => console.log(data));
-    console.log(data)
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+app.get("/", (req, res, next) => {
+    request(options, function(err, output, body) {  
+        var json = JSON.parse(body);
+        console.log(json); // Logging the output within the request function
+        res.json(json) //then returning the response.. The request.json is empty over here
+    });
 });
 
 app.listen(3000, () => {
